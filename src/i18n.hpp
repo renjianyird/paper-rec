3a7d250cc941f7609ed74348f903cbc82dcd4f7d
renjianyird <65107383+renjianyird@users.noreply.github.com>
2026-02-20 15:30:14 +0800

    Update i18n.hpp

--- src/i18n.hpp
+++ src/i18n.hpp
@@ -12,7 +12,6 @@ namespace I18N {
     std::string Translate(const std::string& key);
 }
 
-// 用 inline 函数替代宏，彻底避免循环包含
 inline std::string t(const std::string& key) {
     return I18N::Translate(key);
 }
