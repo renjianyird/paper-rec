7ff1cfe605a9f7953db73533047d168bc3e5c3f1
renjianyird <65107383+renjianyird@users.noreply.github.com>
2026-02-20 15:05:35 +0800

    Update httpRequest.hpp

--- src/httpRequest.hpp
+++ src/httpRequest.hpp
@@ -1,34 +1,20 @@
-// Copyright (C) 2025 Langning Chen
-//
-// This file is part of paper.
-//
-// paper is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// paper is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with paper.  If not, see <https://www.gnu.org/licenses/>.
-
 #pragma once
-
 #include <string>
 #include <map>
+#include <utility>
+#include <stdexcept>
 
-class HTTP_REQUEST
-{
+class HTTP_REQUEST {
 public:
-    HTTP_REQUEST(std::string data);
     std::string method;
     std::string path;
     std::string version;
     std::map<std::string, std::string> headers;
     std::string body;
 
-    static std::pair<size_t, size_t> parseRangeHeader(std::string rangeHeader, size_t fileSize);
+    // 构造函数可能抛出runtime_error，需明确
+    HTTP_REQUEST(std::string data);
+
+    // 解析Range Header，带错误处理
+    std::pair<size_t, size_t> parseRangeHeader(std::string rangeHeader, size_t fileSize);
 };
