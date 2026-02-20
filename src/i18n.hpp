51876e112b727640762c781b05ac158d6547fa16
renjianyird <65107383+renjianyird@users.noreply.github.com>
2026-02-20 14:57:09 +0800

    Update i18n.hpp

--- src/i18n.hpp
+++ src/i18n.hpp
@@ -1,34 +1,9 @@
-// Copyright (C) 2025 Langning Chen
-//
-// This file is part of paper.
-//
-// paper is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// paper is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with paper.  If not, see <https://www.gnu.org/licenses/>.
-
-#pragma once
-
-#include <string>
-#include <unordered_map>
-
-namespace I18N
-{
-    enum class Language
-    {
+namespace I18N {
+    enum class Language {
         ENGLISH,
         CHINESE
     };
-
-    void Initialize();
-    void SetLanguage(Language lang);
+    bool Initialize();
+    void SetCurrentLanguage(Language lang);
+    std::string Translate(const std::string& key);
 }
-std::string t(const std::string &key);
