8898ef0487d93070670fc05713d86cdf78c5eacb
renjianyird <65107383+renjianyird@users.noreply.github.com>
2026-02-20 14:54:58 +0800

    Update httpServer.hpp

--- src/httpServer.hpp
+++ src/httpServer.hpp
@@ -1,49 +1,42 @@
-// Copyright (C) 2025 Langning Chen
-//
-// This file is part of paper.
-//
-// paper is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// paper is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with paper.  If not, see <https://www.gnu.org/licenses/>.
-
 #pragma once
-
 #include <winsock2.h>
+#include <ws2tcpip.h>
 #include <string>
-#include <functional>
 #include <thread>
-#include "httpRequest.hpp"
-#include "httpResponse.hpp"
+#include <map>
+#include <atomic>
 
-class HTTP_SERVER
-{
+// 前置声明
+class HTTP_REQUEST;
+class HTTP_RESPONSE;
+
+class HTTP_SERVER {
 public:
+    // 静态常量（缓冲区大小）
+    static const int BUFFER_SIZE;
+
+    // 构造/析构
     HTTP_SERVER(int port, std::string imagePath, std::string otaData, std::string otaUrl);
+    ~HTTP_SERVER() {
+        if (isRunning) {
+            stop();
+        }
+    }
+
+    // 核心方法
     void start();
     void stop();
+    void handleRequest(SOCKET clientSocket, HTTP_REQUEST request);
+    void sendHttpResponse(SOCKET clientSocket, int statusCode, std::string contentType,
+                          std::string body, std::string extraHeaders = "");
+    void sendFileResponse(SOCKET clientSocket, std::map<std::string, std::string> headers);
 
 private:
-    void handleRequest(int clientSocket, HTTP_REQUEST request);
-
-    void sendHttpResponse(int clientSocket, int statusCode, std::string contentType,
-                          std::string body, std::string extraHeaders = "");
-    void sendFileResponse(int clientSocket, std::map<std::string, std::string> headers);
-
     int serverPort;
     std::string imagePath;
     std::string otaData;
     std::string otaUrl;
-    std::thread serverThread;
-    bool isRunning;
+    std::atomic<bool> isRunning; // 原子变量，保证线程安全
     SOCKET serverSocket;
-    static const int BUFFER_SIZE = 8192;
+    std::thread serverThread;
 };
